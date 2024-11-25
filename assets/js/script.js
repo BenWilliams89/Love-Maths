df4ffbeccbc3a878876e0ed39649f0df0539b176
Ben Williams <codebencode@icloud.com>
2024-11-25 15:12:08 +0000

    Add division change and completed project

--- assets/js/script.js
+++ assets/js/script.js
@@ -50,8 +50,11 @@ if (gameType === 'addition') {
     displayMultiplyQuestion(num1, num2)
 } else if (gameType === 'subtract') {
     displaySubtractQuestion(num1, num2)
-} else {
+} else if (gameType === "division") {
     displayDivisionQuestion(num1, num2)
+} else {
+    alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
 }
 }
 
@@ -103,8 +106,12 @@ function calculateCorrectAnswer() {
     } else if (operator === 'x') {
         return [operand1 * operand2, 'multiply'];
 
+    } else if (operator === '/') {
+    return [operand1 / operand2, 'division'];
+
     } else {
-    return [operand1 / operand2, 'divide'];
+        alert(`Unknown game type: ${operator}`);
+        throw `Unknown game type: ${operator}. Aborting!`;
     }
 }
 
@@ -158,7 +165,8 @@ function displayMultiplyQuestion(operand1, operand2) {
 //function for displayDivisionQuestion
 
 function displayDivisionQuestion(operand1, operand2) {
-    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
-    document.getElementById('operand2').textContent = operand2 > operand2 ? operand2 : operand1;
+    operand1 = operand1 * operand2;
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = '/';
 }
