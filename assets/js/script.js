1b2e5844fec99a51b39c3849c43153c4a926c6a7
Ben Williams <codebencode@icloud.com>
2024-11-25 10:51:40 +0000

    Add functions required steps

---
+++ assets/js/script.js
@@ -0,0 +1,57 @@
+
+
+
+
+// function for run game
+
+function runGame() {
+
+}
+
+// function for check answers
+
+function checkAnswer() {
+
+}
+
+// function for calculateCorrectAnswer
+
+function calculateCorrectAnswer() {
+
+}
+
+// function for incrementScore
+
+function incrementScore(){
+
+}
+
+// function for incrementWrongAnswer
+
+function incrementWrongAnswer() {
+
+}
+
+//function for displayAdditionQuestion
+
+function displayAdditionQuestion() {
+
+}
+
+//function for displaySubtractQuestion
+
+function displaySubtractQuestion() {
+
+}
+
+//function for displayMultiplyQuestion
+
+function displayMultiplyQuestion() {
+
+}
+
+//function for displayDivisionQuestion
+
+function displayDivisionQuestion() {
+    
+}
