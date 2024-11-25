b85c3f3f9b2dcdc0ea0015788b635aa7013a71f3
Ben Williams <codebencode@icloud.com>
2024-11-25 12:04:47 +0000

    Add addition questions to the page when it loads

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,10 +14,13 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert('You clicked Submit!')
             } else {
                 let gameType = this.getAttribute('data-type');
-                alert(`you clicked ${gameType}`)
+                runGame(gameType)
             }
         })
     }
+
+    // run this when page is finsihed loading as our default game option
+    runGame('addition');
 })
 
 /**
@@ -26,10 +29,18 @@ document.addEventListener("DOMContentLoaded", function() {
  */
 
 // function for run game
-function runGame() {
+function runGame(gameType) {
  // creates two random numbers between 1 & 25 - the +1 removes 0 as a possible number
 let num1 = Math.floor(Math.random() * 25) + 1
 let num2 = Math.floor(Math.random() * 25) + 1
+
+if (gameType === 'addition') {
+    displayAdditionQuestion(num1, num2)
+} else {
+    alert(`Unknown game type: ${gameType}`);
+    throw `Unknow game Type: ${gameType}. Aborting!`
+}
+
 }
 
 
@@ -60,8 +71,10 @@ function incrementWrongAnswer() {
 
 //function for displayAdditionQuestion
 
-function displayAdditionQuestion() {
-
+function displayAdditionQuestion(operand1, operand2) {
+document.getElementById('operand1').textContent = operand1;
+document.getElementById('operand2').textContent = operand2;
+document.getElementById('operator').textContent = '+';
 }
 
 //function for displaySubtractQuestion
